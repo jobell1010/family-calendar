a7b9603a73b81bad591bb38507c421b88c565487
jobell1010 <48512499+jobell1010@users.noreply.github.com>
2023-09-15 14:31:22 +0100

    playing with event format
    Found out about the field names for each event.

--- calendar/get_calendar.py
+++ calendar/get_calendar.py
@@ -1,8 +1,7 @@
 from icalevents.icalevents import events
 
-es = events("https://calendar.google.com/calendar/ical/jo-bell%40live.co.uk/"
-            "private-1663142b1daf9a471001bf725ef93324/basic.ics")
+events = events("https://calendar.google.com/calendar/ical/jo-bell%40live.co.uk/"
+                "private-1663142b1daf9a471001bf725ef93324/basic.ics")
 
-for i in es:
-    print(i)
-    
+for individual_event in events:
+    print(individual_event.summary, individual_event.start, individual_event.end)
