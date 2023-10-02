9b4915f6708d280eb744d6d27943b1627d66b383
jobell1010 <48512499+jobell1010@users.noreply.github.com>
2023-10-02 11:04:27 +0100

    making the calendar information display in a webpage
    Figuring out how to link the stuff all together.

--- calendar/get_calendar.py
+++ calendar/get_calendar.py
@@ -1,12 +1,25 @@
 from icalevents.icalevents import events
+from flask import Flask
 
-ev_list = events(file="basic.ics", sort=True)
+app = Flask(__name__)
 
-for individual_event in ev_list:
-    start = individual_event.start
 
-    print(individual_event.summary)
-    print(individual_event.start)
-    print(individual_event.end)
-    print()
-    
+def get_events():
+    items = []
+    ev_list = events(file="basic.ics", sort=True)
+    for individual_event in ev_list:
+        # print(individual_event.summary)
+        # print(individual_event.start)
+        # print(individual_event.end)
+        event_title = individual_event.summary
+        items.append(event_title)
+    return items
+
+
+# event_names = get_events()
+# print(event_names)
+
+@app.route('/')
+def calendar():
+    event = get_events()
+    return event
