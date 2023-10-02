1c02b9556f769e7380a8c93d75c33e72c4b5868b
jobell1010 <48512499+jobell1010@users.noreply.github.com>
2023-10-02 13:31:58 +0100

    make calendar webpage

--- calendar/get_calendar.py
+++ calendar/get_calendar.py
@@ -1,5 +1,6 @@
 from icalevents.icalevents import events
-from flask import Flask
+from datetime import datetime
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -8,18 +9,16 @@ def get_events():
     items = []
     ev_list = events(file="basic.ics", sort=True)
     for individual_event in ev_list:
-        # print(individual_event.summary)
-        # print(individual_event.start)
-        # print(individual_event.end)
         event_title = individual_event.summary
-        items.append(event_title)
+        start = individual_event.start
+        st_time = start.strftime("Start:  %A %d %B, %H:%M")
+        end = individual_event.end
+        end_time = end.strftime("End:  %A %d %B, %H:%M")
+        items.append([event_title, st_time, end_time])
     return items
 
 
-# event_names = get_events()
-# print(event_names)
-
 @app.route('/')
 def calendar():
-    event = get_events()
-    return event
+    event_list = get_events()
+    return render_template('calendar.html', event_list=event_list)
